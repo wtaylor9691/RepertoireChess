db071de546762a47cc220731d0d2f464c6b6f255
Winslow <winslow.benjamin.taylor@gmail.com>
2022-04-05 18:50:38 -0500

    express server files added

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express(); 
+const PORT = 8080; 
+const path = require('path'); 
+const cookieParser = require('cookie-parser');
+
--- src/components/containers/OpeningContainer.jsx
+++ src/components/containers/OpeningContainer.jsx
@@ -1,8 +1,15 @@
+import React from 'react'; 
+import { useState } from 'react'; 
+import Chessboard from 'chessboardjsx'; 
+
+
+
 function OpeningContainer(props) {
-    //fetches for the openings (may) go here in the container
+    //fetches for the openings to the backend (may) go here in the container 
     return (
         <div>
             <div>Opening Box</div>
+            <p id="demo" onclick="myFunction()">click here </p>
         </div>
     );
 }
